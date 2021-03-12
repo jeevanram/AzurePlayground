c9d3ea0801956f0daaeff602cb93d62f5b9ed98b
Jeevanram <mailjeevanram@gmail.com>
2021-03-11 18:49:09 -0600

    AzureApp : Webhook with Github

--- DemoAzureFunctionApp/DemoAzureFunctionApp/WebhookTriggeredFunction.cs
+++ DemoAzureFunctionApp/DemoAzureFunctionApp/WebhookTriggeredFunction.cs
@@ -18,17 +18,17 @@ namespace DemoAzureFunctionApp
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            var reader = new StreamReader(req.Body);
+            reader.BaseStream.Seek(0, SeekOrigin.Begin);
+            dynamic requestBody = JsonConvert.DeserializeObject(reader.ReadToEnd());
+            dynamic pages = requestBody.pages;
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            if (pages != null)
+            {
+                dynamic pageInfo = pages[0];
+                return (ActionResult)new OkObjectResult($"Page Details: Title-{pageInfo.title}, Action is-{pageInfo.action}, HtmlURL is-{pageInfo.html_url}");
+            }
+            return (ActionResult)new BadRequestObjectResult("Invalid request !!!");
         }
     }
 }
